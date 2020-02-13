31c3e33c5948e6160db86fda358284e296cb4305
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2020-02-12 21:31:35 -0300

    Chore(boilerplate): initial commit boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base', "prettier"],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ["prettier"],
+  rules: {
+    camelcase: 'off',
+    'class-methods-use-this': 'off',
+    'prettier/prettier': 'error',
+    'no-param-reassign': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
